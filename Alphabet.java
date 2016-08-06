f14a519efca564a4a1a1ed745f09a3857ff96342
Mohanapriyaa <priyaannakodi1996@gmail.com>
2016-08-06 17:50:16 +0530

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,17 @@
+
+public class Alphabet {
+
+	public static void main(String[] args) {
+ char n='h';
+if((n>64&&n<91)||(n>96 && n<123)) 
+{
+System.out.println("Alphabet");
+}
+else
+{
+	System.out.println("Not alphabet");
+}
+
+	}
+
+}
