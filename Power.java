9c4c4aced4b4db2be3166d129bbeb8bbe5766a60
Mohanapriyaa <priyaannakodi1996@gmail.com>
2016-08-06 19:02:28 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Power {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+     int a=9;
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     int output1=(int) Math.pow(a,n);
+     System.out.println(output1);
+     
+	}
+
+}
